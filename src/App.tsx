10b3dca1548e149ac9fa3d086b4ab3047a045a7f
Jairo <jespinozac96@gmail.com>
2026-06-02 22:02:55 -0500

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { HomePage } from './hm-refrigeracion/pages/home-page/Home-page'
+
+function App() {
+
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/hm-refrigeracion/components/carrousel/Carrousel.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+
+
+export const Carrousel = () => {
+  return (
+    <>
+        <h1>IMAGEN</h1>
+    </>
+  )
+}
